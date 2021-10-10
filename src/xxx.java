810d31df7b13992871f7eb6d663caba6bd230ad5
unknown <igatatsu1997@gmail.com>
2021-10-10 12:05:36 +0900

    commit

---
+++ src/xxx.java
@@ -0,0 +1,12 @@
+
+public class xxx {
+
+	public static void main(String[] args) {
+		System.out.println("Hello!");
+		System.out.println("MY name is Igaki Tatsushi.");
+		System.out.println("I live in Himeji");
+		System.out.println("Thank you!");
+		
+	}
+
+}
